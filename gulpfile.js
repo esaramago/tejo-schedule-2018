a0f01ecee6e370f87ee2fe6e3fc175017628315d
Emanuel Saramago <emanuel.reg@outlook.com>
2019-04-07 22:32:37 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,151 @@
+
+// Dependencies =================================================
+const { series, src, dest } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const sassGlob = require('gulp-sass-glob');
+const cssmin = require('gulp-cssmin');
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require('gulp-data');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+
+const browserify = require('browserify');
+const envify = require('envify/custom');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+
+
+// Settings ====================================================
+const srcPath = './src';
+const htmlPath = srcPath + '/html';
+const stylesPath = srcPath + '/scss';
+const scriptsPath = srcPath + '/js';
+const imagesPath = srcPath + '/img';
+
+const destPath = 'app';
+
+const isProd = process.env.Node_ENV === 'production';
+
+if (isProd) {
+    console.log('Running in production mode.')
+}
+else if (process.env.NODE_ENV === "development") {
+    console.warn('Running in DEVELOPMENT mode.')
+}
+
+// Tasks =======================================================
+
+// delete dist files and folders
+function cleanDist() {
+    return gulp.src(destPath + '/*', { read: false })
+        .pipe(clean());
+}
+gulp.task('cleanDist', cleanDist);
+
+
+function html() {
+
+    return src(htmlPath + '/*.njk')
+        .pipe(plumber())
+        .pipe(data(function() {
+            return require(srcPath + '/data/data.json')
+        }))
+        .pipe(nunjucksRender({
+            path: srcPath
+        }))
+        .pipe(rename(path => path.extname = ".html"))
+        .pipe(gulp.dest(destPath));
+}
+gulp.task('html', html);
+
+
+function styles() {
+
+    return src(stylesPath + '/main.scss')
+
+        .pipe(plumber())
+
+        .pipe(sourcemaps.init())
+
+        .pipe(sassGlob())
+        .pipe(sass().on('error', sass.logError))
+
+        .pipe(cssmin())
+
+        .pipe(sourcemaps.write(''))
+
+        .pipe(dest(destPath));
+}
+gulp.task('styles', styles);
+
+
+function scripts() {
+    return browserify(scriptsPath + '/main.js')
+        .transform(
+            babelify,
+            { presets: ['@babel/env'] }, // configured also in package.json
+        )
+        .transform(
+            { global: true },
+            envify({ NODE_ENV: process.env.NODE_ENV })
+        )
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+
+        // minify
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify({
+            compress: { drop_debugger: isProd }
+        }))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest(destPath));
+}
+gulp.task('scripts', scripts);
+
+
+function images() {
+    return src(imagesPath + '/*')
+        .pipe(plumber())
+        .pipe(dest(destPath + '/img'));
+}
+gulp.task('images', images);
+
+
+function root() {
+    // copy root files
+    return src(srcPath + '/*.{js,json}')
+        .pipe(plumber())
+        .pipe(dest(destPath));
+}
+gulp.task('root', root);
+
+
+// Development (gulp watch) ======================================================
+function watch() {
+    gulp.watch(htmlPath + '/**/*.njk', gulp.series('html'));
+    gulp.watch(stylesPath + '/**/*.scss', gulp.series('styles'));
+    gulp.watch(scriptsPath + '/**/*.js', gulp.series('scripts'));
+    gulp.watch(imagesPath + '/*', gulp.series('images'));
+}
+gulp.task('watch', watch);
+
+
+// Production (gulp build) ======================================================
+exports.build = series(
+    'cleanDist',
+    'html',
+    'styles',
+    'scripts',
+    'images',
+    'root'
+);
+
+
+
+exports.default = watch;
---
+++ src/js/main.js
@@ -0,0 +1,181 @@
+
+import data from '../data/data.json';
+
+const App = {
+    Data: data,
+    Now: new Date,
+    PageId: null,
+    Schedules: null,
+    Schedule: null,
+
+    Current: {
+        DayOfWeek: '',
+        Time: null
+    },
+
+    selectedDayOfWeek: null,
+
+    DOM: {
+        Pages: {
+            Home: document.getElementById('homePage'),
+            Schedule: document.getElementById('schedulePage')
+        },
+        //Itineraries: document.querySelector('.js-itineraries'),
+        Title: document.querySelector('.js-title'),
+        Schedules: document.querySelectorAll('.js-schedule'),
+        Tabs: document.querySelectorAll('.js-tab'),
+        TabPanels: document.querySelectorAll('.js-tabpanel')
+    },
+
+    init() {
+
+        // Settings
+        this.PageId = document.body.dataset.pageId;
+        this.Schedules = this.Data[this.PageId];
+
+        this.getDayOfWeek();
+        this.getNextTime();
+        this.createNavigation();
+    },
+
+    // GET/SET
+    getDayOfWeek() {
+
+        var hours = this.Now.getHours();
+        var isAfterMidnight = hours >= 0 && hours < 5;
+
+        // get weekday (sunday is 0, monday is 1, and so on.)
+        var weekday = this.Now.getDay();
+        if (isAfterMidnight) {
+
+            // Fix current weekday - make today yesterday
+            weekday = weekday - 1;
+            this.Now.setDate(this.Now.getDate() - 1);
+
+            // if sunday gets a negative value, make it saturday
+            if (weekday == -1) {
+                weekday = 6;
+            }
+        }
+
+        var dayOfWeek;
+        if (weekday >= 1 && weekday <= 5) {
+            dayOfWeek = 'weekday';
+        }
+        else if (weekday == 6) {
+            dayOfWeek = 'saturday';
+        }
+        else if (weekday == 0) {
+            dayOfWeek = 'sunday';
+        }
+
+        this.Current.DayOfWeek = dayOfWeek;
+        this.selectedDayOfWeek = dayOfWeek;
+    },
+    getNextTime() {
+        
+        var time = this.Now.getTime();
+        
+        this.Schedules.forEach(schedule => {
+            var schedule = schedule.days.find(day => day.day === this.Current.DayOfWeek).schedule;
+
+            schedule.forEach(time => {
+                var dateTime = new Date(time)
+            });
+            debugger
+        })
+    },
+
+    // EVENTS
+    createNavigation() {
+
+        document.addEventListener('click', (event) => {
+
+            var clickedElement = event.currentTarget.activeElement;
+
+            if (clickedElement.matches('.js-goto-schedule')) {
+                var way = clickedElement.dataset.way;
+                this.goToSchedulePage(way);
+            }
+            else if (clickedElement.matches('.js-goto-home')) {
+                this.goToHomePage();
+            }
+            else if (clickedElement.matches('.js-tab')) {
+                this.selectedDayOfWeek = clickedElement.dataset.dayofweek;
+                this.selectTab();
+            }
+
+        }, false);
+    },
+
+
+    // DOM MANIPULATION
+    goToSchedulePage(way) {
+
+        this.Schedule = this.Schedules.find(x => x.way === way);
+
+        this.renderTitle();
+        this.renderSchedule();
+        this.selectTab();
+
+        // close Home page tab
+        this.DOM.Pages.Home.setAttribute("aria-hidden", true);
+
+        // open Schedule page
+        this.DOM.Pages.Schedule.classList.add("is-active");
+        this.DOM.Pages.Schedule.setAttribute("aria-hidden", false);
+
+        this.scrollToCurrentTime();
+    },
+    goToHomePage() {
+        this.DOM.Pages.Home.setAttribute("aria-hidden", false);
+
+        this.DOM.Pages.Schedule.classList.remove("is-active");
+        this.DOM.Pages.Schedule.setAttribute("aria-hidden", true);
+
+        // reset tab selection
+        this.selectedDayOfWeek = this.Current.DayOfWeek;
+    },
+    selectTab() {
+
+        // hide tabs
+        this.DOM.Tabs.forEach(tab => {
+            tab.setAttribute("aria-selected", false);
+        });
+        this.DOM.TabPanels.forEach(panel => {
+            panel.setAttribute("aria-hidden", true);
+        });
+
+        // show tab
+        var attr = `[data-dayofweek="${this.selectedDayOfWeek}"]`;
+        document.querySelector(`.js-tab${attr}`).setAttribute("aria-selected", true);
+        document.querySelector(`.js-tabpanel${attr}`).setAttribute("aria-hidden", false);
+        
+    },
+    scrollToCurrentTime() {
+
+    },
+
+
+    // RENDER
+    renderTitle() {
+        this.DOM.Title.textContent = `${this.Schedule.departure} - ${this.Schedule.arrival}`;
+    },
+    renderSchedule() {
+        this.DOM.Schedules.forEach((schedule, i) => {
+            var html = '';
+            this.Schedule.days[i].schedule.forEach((time, j) => {
+                var isNext = null;
+                html += `
+                    <li ${isNext ? 'class="is-next"' : ''}>
+                        <span>${time.hour}:${time.minute}</span>
+                    </li>
+                `;
+            });
+            this.DOM.Schedules[i].innerHTML = html;
+        });
+    }
+}
+
+App.init();
+window.App = App;
